aba7e30c1be6a2c974b943b234222493e541e1b9
alexossipovDOMA <alexossipovv991@gmail.com>
2024-11-11 14:06:53 +0300

    change footer text

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
   return (
     <footer>
-        Все права защищены &copy;
+        Все права спиздали с левого сайта &copy;
     </footer>
   )
 }
